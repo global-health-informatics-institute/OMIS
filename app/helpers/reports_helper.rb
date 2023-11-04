b2ca40afcddb5fae06f3ff4174f42e8eaeb3d03d
Timothy Mtonga <timmtonga@gmail.com>
2023-11-04 15:53:59 +0200

    Added turbo stream for report viewing

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,10 @@
 module ReportsHelper
+  def select_report
+    options = [['HR Report', 'HR'], ['Asset Status', 'Assets'],
+               ['Monthly Individual LOE Report', 'Employee LOE'],
+               ['Monthly Organization LOE Report', 'Org LOE'],
+               ['Project Progress Report','Project Report']
+    ]
+    return options
+  end
 end
