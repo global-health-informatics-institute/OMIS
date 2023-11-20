fbf08dde88931edd3003262930a75f521e664d5b
Timothy Mtonga <timmtonga@gmail.com>
2023-11-20 15:41:33 +0200

    configured for production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,12 @@ class UsersController < ApplicationController
 
   def password_reset
     if request.method.to_s == "POST"
-      user = User.find(params[:id]).update(password: params[:user][:password], reset_needed: false)
-      redirect_to "/"
+      if params[:user][:password] == params[:user][:password_confirmation]
+        user = User.find(params[:id]).update(password: params[:user][:password], reset_needed: false)
+        redirect_to "/"
+      else
+        flash[:error] = "Passwords mismatch"
+      end
     end
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def leave_types
+    options = ['Annual Leave', 'Compassionate Leave','Maternity Leave','Paternity Leave',
+               'Sick Leave', 'Study Leave','Unpaid Leave']
+    return options
+  end
 end
