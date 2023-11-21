c99dc16220f6c25b0f686f7d3651c5ce37a61426
Timothy Mtonga <timmtonga@gmail.com>
2023-11-21 08:23:36 +0200

    Updated paths to timesheets

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,8 +4,8 @@ class MainController < ApplicationController
     if current_user
       @employee = current_user.employee
       @person = @employee.person
-      @current_timesheet = Timesheet.where(employee_id: @employee.id,
-                                           timesheet_week: Date.today.beginning_of_week).first_or_create
+      #@current_timesheet = Timesheet.where(employee_id: @employee.id,
+      #                                     timesheet_week: Date.today.beginning_of_week).first_or_create
       @upcoming_deadlines = ProjectTask.where("project_task_id in (?)",
                                               ProjectTaskAssignment.select(:project_task_id).where(assigned_to: current_user.employee_id,
                                                                           revoked: false )
@@ -17,6 +17,7 @@ class MainController < ApplicationController
         "task_date between ? and ? ", Date.today.at_beginning_of_month, Date.today.at_end_of_month).group(:project_id)
       @current_prj_names = Project.select(:short_name).where(project_id: @loe_current.collect { |x| x.project_id })
 
+=begin
       #The next block will need to be put in a function as it is repeated elsewhere
       @records = {}
       records = @current_timesheet.timesheet_tasks.select("project_id, task_date,description, sum(duration) as duration").group(
@@ -28,8 +29,9 @@ class MainController < ApplicationController
         end
         @records[v.project_id][v.description][v.task_date.cwday] = v.duration
       end
-
       @projects = Project.where(project_id: records.collect{|p| p.project_id}.uniq).collect { |x| [x.project_id, x.short_name] }.to_h
+=end
+
 
     else
       people = Employee.where(still_employed: true).pluck(:person_id)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,9 @@ module ApplicationHelper
                'Sick Leave', 'Study Leave','Unpaid Leave']
     return options
   end
+
+  def current_timesheet
+    Timesheet.where(employee_id: @current_user.employee.id,
+                    timesheet_week: Date.today.beginning_of_week).first_or_create
+  end
 end
