944227bf12f638dde46eca2d82e7f388ef7dfdab
Timothy Mtonga <timmtonga@gmail.com>
2023-11-23 13:58:01 +0200

    Added monthly LOE report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -14,7 +14,7 @@ class ReportsController < ApplicationController
     when 'Employee LOE'
       monthly_employee_loe_report()
     when 'Org LOE'
-      monthly_employee_loe_report
+      monthly_org_loe_report
     when 'Project Report'
       project_progress_report
     end
@@ -69,6 +69,34 @@ class ReportsController < ApplicationController
       else
         check = @daily_summary[record.project_id][record.task_date.day]
         @daily_summary[record.project_id][record.task_date.day] = (check.blank? ? record.duration.to_f.floor(2) : (check + record.duration.to_f.floor(2)))
+      end
+    end
+  end
+
+  def monthly_org_loe_report
+    summaries = Timesheet.find_by_sql("select CONCAT(first_name,' ' ,last_name) as person_name, person_id, short_name,
+employee_id, projects.project_id, duration from people inner join (Select employee_id, project_id, sum(duration) as duration
+from timesheets t inner join timesheet_tasks tt on
+t.timesheet_id = tt.timesheet_id where task_date
+BETWEEN '2023-10-26' and '2023-11-25' GROUP BY employee_id, project_id) as summary
+on summary.employee_id = people.person_id inner join projects
+on projects.project_id = summary.project_id")
+
+    @projects_array = summaries.collect{|x| x['short_name']}.uniq
+    @results = {}
+
+    (summaries || []).each do |summary|
+      if @results[summary['person_name']].blank?
+        @results[summary['person_name']] = Hash[ *@projects_array.collect { |v| [ v, 0.0 ] }.flatten]
+      end
+
+      @results[summary['person_name']][summary['short_name']] = summary['duration']
+    end
+    respond_to do |format|
+      format.turbo_stream
+      format.xsl do
+        helpers.monthly_loe_report(@results, @projects_array)
+        send_file("tmp/loe_report.xls", :filename => "nove_loe.xls")
       end
     end
   end
--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -7,4 +7,30 @@ module ReportsHelper
     ]
     return options
   end
+
+  def monthly_loe_report(records, projects)
+    book = Spreadsheet::Workbook.new # We have created a new object of the Spreadsheet book
+
+    sheet = book.create_worksheet(name: 'First sheet') # We are creating new sheet in the Spreadsheet(We can create multiple sheets in one Spreadsheet book)
+
+    # syntax to create new row is as the following:
+    # sheet.row(row_number).push(column first', 'column second', 'column third')
+
+    sheet.row(0).push('Employee Name')
+    (projects || []).each do |project|
+      sheet.row(0).push(project)
+    end
+
+    i = 1
+    (records || []).each do |k, v|
+      sheet.row(i).push(k)
+      (projects || []).each do |project|
+        sheet.row(i).push(v[project])
+      end
+      i+=1
+    end
+    # Write this sheet's contain to the test.xls file.
+    book.write 'tmp/loe_report.xls'
+
+  end
 end
