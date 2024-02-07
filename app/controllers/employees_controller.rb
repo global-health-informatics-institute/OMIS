4ee99cc32753c1c96bc2648e474961e46dfdea79
Thembi01 <sithembiphombeya@gmail.com>
2024-02-07 11:31:05 +0200

    added directory page

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,11 @@
 class EmployeesController < ApplicationController
   def index
+    @list_employees = Person.select(:primary_phone, :alt_phone, :first_name, :last_name, :email_address)
+    #@q = Person.ransack(params[:q])
+    #@list_employees = @q.result(distinct: true)
+  end
+
+  def show 
   end
 
   def new
