829e64eb6acf6f694d78dba0fb68d6c1ef6fa14a
Thembi01 <sithembiphombeya@gmail.com>
2024-05-16 13:54:29 +0200

    added inventory page

--- app/controllers/inventory_items_controller.rb
+++ app/controllers/inventory_items_controller.rb
@@ -12,8 +12,13 @@ class InventoryItemsController < ApplicationController
   end
 
   def show
-    @inventory_items = InventoryItem.find_by_sql("SELECT inventory_item_id, storage location from inventoryitem ")
-    @inventory_item_category = InventoryItemCategory.all
+    @inventory_items = InventoryItem.find_by_sql("SELECT * from inventory_items as ii inner join (select
+                      inventory_item_type_id, inventory_item_category_id, item_name from
+                      inventory_item_types) as iit on ii.item_type_id = iit.inventory_item_type_id
+                      inner join (select inventory_item_category_id, category from inventory_item_categories)
+                      as iic on iit.inventory_item_category_id = iic.inventory_item_category_id")
+    #raise @inventory_items.inspect
+    #@inventory_item_category = InventoryItemCategory.all
   end
 
   def edit
