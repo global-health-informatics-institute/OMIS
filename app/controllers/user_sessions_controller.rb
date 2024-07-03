2758798b16d36a19c5c09defeb1d40e76c7a88e4
timmtonga <timmtonga@gmail.com>
2024-07-03 12:28:25 +0200

    Tighted authenication for employees that have left

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,14 +8,20 @@ class UserSessionsController < ApplicationController
     @user = User.find_by_username(params[:user][:username])
 
     if @user && @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      if @user.reset_needed
-        redirect_to "/users/#{@user.user_id}"
-        flash[:info] = "Please reset password."
+      if @user.activated
+        session[:user_id] = @user.id
+        if @user.reset_needed
+          redirect_to "/users/#{@user.user_id}"
+          flash[:info] = "Please reset password."
+        else
+          flash[:notice] = "Successfully logged in."
+          redirect_to root_path
+        end
       else
-        flash[:notice] = "Successfully logged in."
-        redirect_to root_path
+        flash[:error] = "Your user account was deactivated. Please contact your supervisor"
+        redirect_to new_user_session_path  
       end
+      
     else
       flash[:error] = "Login failed. Wrong password/username"
       redirect_to new_user_session_path
