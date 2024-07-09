44a43796e0e4d82d2b17ccc95ce3f8344b6b0bd9
Thembi01 <sithembiphombeya@gmail.com>
2024-07-09 10:22:35 +0200

    fixed total_hours_worked variable

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,7 +9,7 @@ class MainController < ApplicationController
                                          .where("employee_id in (?) and submitted_on is NULL", @employee.id).length
       @unused_leave = LeaveSummary.where(employee_id: @employee.id, leave_type: 'Annual Leave',
                                          financial_year: Date.today.year).first
-      
+
       @upcoming_deadlines = ProjectTask.where("project_task_id in (?)",
                                               ProjectTaskAssignment.select(:project_task_id).where(assigned_to: current_user.employee_id,
                                                                           revoked: false )
