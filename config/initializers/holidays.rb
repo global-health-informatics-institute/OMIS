e0432cae6d195f8b32540df449842c1646e231d0
timmtonga <timmtonga@gmail.com>
2024-10-07 09:03:35 +0200

    Made fixes to leave requests

--- config/initializers/holidays.rb
+++ config/initializers/holidays.rb
@@ -1,3 +1,5 @@
 file_path = File.join(Rails.root, 'config', 'holidays', 'mw.yml')
 
-Holidays.load_custom(file_path)
+if File.file?(file_path)
+  Holidays.load_custom(file_path)
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -151,35 +151,35 @@ CSV.foreach("#{source}/inventory_item.csv",:headers=>:true) do |row|
   InventoryItem.create( item_type_id: row[0], quantity: row[1], quantity_used: row[2], supplier: row[3],
                         unit_price: row[4], storage_location: row[5], created_by: row[6])
 end
-#
-# puts 'Adding Workflows and States'
-# CSV.foreach("#{source}/workflow_processes.csv",:headers=>:true) do |row|
-#   wp = WorkflowProcess.where(workflow: row[0]).first_or_create
-#   wfs = WorkflowState.where(workflow_process_id: wp.id, state: row[1], description: row[2]).first_or_create
-#
-#   if row[3].upcase.strip == 'TRUE'
-#     puts "Adding initial state"
-#     InitialState.create(workflow_process_id: wp.id, workflow_state_id: wfs.id)
-#   end
-# end
-#
-# puts 'Adding workflow transitions'
-# CSV.foreach("#{source}/workflow_state_transitions.csv",:headers=>:true) do |row|
-#   wp = WorkflowProcess.where(workflow: row[0]).first_or_create
-#   wps_1 = WorkflowState.where(workflow_process_id: wp.id, state: row[1]).first_or_create
-#   wps_2 = WorkflowState.where(workflow_process_id: wp.id, state: row[2]).first_or_create
-#   wft = WorkflowStateTransition.where(workflow_state_id: wps_1, next_state: wps_2, action:row[3],
-#                                 by_owner: (row[4].upcase == "TRUE" ? true : false),
-#                                 by_supervisor: (row[5].upcase == "TRUE" ? true : false),
-#                                 ).first_or_create
-#
-#   if !row[6].blank?
-#     (row[6].split(';')).each do |transitioner|
-#       WorkflowStateActor.create(workflow_state_transition_id: wft.id,
-#                              employee_designation_id: Designation.find_by_designated_role(transitioner).id)
-#     end
-#   end
-# end
+
+ puts 'Adding Workflows and States'
+ CSV.foreach("#{source}/workflow_processes.csv",:headers=>:true) do |row|
+   wp = WorkflowProcess.where(workflow: row[0]).first_or_create
+   wfs = WorkflowState.where(workflow_process_id: wp.id, state: row[1], description: row[2]).first_or_create
+
+   if row[3].upcase.strip == 'TRUE'
+     puts "Adding initial state"
+     InitialState.create(workflow_process_id: wp.id, workflow_state_id: wfs.id)
+   end
+ end
+
+ puts 'Adding workflow transitions'
+ CSV.foreach("#{source}/workflow_state_transitions.csv",:headers=>:true) do |row|
+   wp = WorkflowProcess.where(workflow: row[0]).first_or_create
+   wps_1 = WorkflowState.where(workflow_process_id: wp.id, state: row[1]).first_or_create
+   wps_2 = WorkflowState.where(workflow_process_id: wp.id, state: row[2]).first_or_create
+   wft = WorkflowStateTransition.where(workflow_state_id: wps_1, next_state: wps_2, action:row[3],
+                                 by_owner: (row[4].upcase == "TRUE" ? true : false),
+                                 by_supervisor: (row[5].upcase == "TRUE" ? true : false),
+                                 ).first_or_create
+
+   if !row[6].blank?
+     (row[6].split(';')).each do |transitioner|
+       WorkflowStateActor.create(workflow_state_transition_id: wft.id,
+                              employee_designation_id: Designation.find_by_designated_role(transitioner).id)
+     end
+   end
+ end
 
 puts 'Seeding database done'
 
