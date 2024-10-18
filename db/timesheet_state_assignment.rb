1bd5fda7214d84ee156e7b81ccc271ceadab8f84
Thembi01 <sithembiphombeya@gmail.com>
2024-10-18 09:27:00 +0200

    updated state assignment script

--- db/timesheet_state_assignment.rb
+++ db/timesheet_state_assignment.rb
@@ -3,9 +3,9 @@ def main()
 end
 
 def assign_timesheet_state()
-  unsubmitted = Timesheet.where(submitted_on: nil, approved_by: nil, approved_on: nil)
-  submitted = Timesheet.where.not(submitted_on: nil).where(approved_by: nil, approved_on: nil)
-  approved = Timesheet.where.not(submitted_on: nil, approved_by: nil, approved_on: nil)
+  unsubmitted = Timesheet.where(submitted_on: nil, approved_by: nil, approved_on: nil, state: nil)
+  submitted = Timesheet.where.not(submitted_on: nil).where(approved_by: nil, approved_on: nil, state: nil)
+  approved = Timesheet.where.not(submitted_on: nil, approved_by: nil, approved_on: nil).where(state: nil)
 
   unsubmitted.update(state: 6)
   submitted.update(state: 7)
