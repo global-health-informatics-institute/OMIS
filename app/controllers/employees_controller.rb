4b9d6dbc33ea61833ec18b4910a65d769c7b6fce
Thembi01 <sithembiphombeya@gmail.com>
2024-11-29 08:14:21 +0200

    editing employees + restricted access

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,9 +1,10 @@
 class EmployeesController < ApplicationController
+  before_action :can_access?, only: [:create, :edit, :update, :show, :index]
   def index
     @list_employees = Employee.where(still_employed: true).collect { |x| x.person }
   end
 
-  def show 
+  def show
   end
 
   def new
@@ -13,7 +14,8 @@ class EmployeesController < ApplicationController
   end
 
   def create
-    selected_supervisor = employee_params[:supervisor].split(' ')
+    raise params.inspect
+    selected_supervisor = params[:supervision][:supervisor].split(' ')
     supervisor_id = Person.where(first_name: selected_supervisor[0], last_name: selected_supervisor[1])
                           .pluck(:person_id).first
     designated_role = Designation.where(designated_role: employee_params[:designated_role])
@@ -73,8 +75,39 @@ class EmployeesController < ApplicationController
   end
 
   def edit
+    @user = @current_user
+    @person = Person.find(params[:id])
+    @employee = Employee.find(params[:id])
+    @project = Project.all.collect{|x| [x.project_name, x.id]}
   end
 
+  def update
+    @person = Person.find(params[:id])
+    @employee = Employee.find(params[:id])
+    @designated_role = Designation.where(designated_role: employee_params[:designated_role])
+    if @person.update(first_name: params[:person][:first_name], middle_name: params[:person][:middle_name],
+                      last_name: params[:person][:last_name],
+                      primary_phone: params[:person][:primary_phone], alt_phone: params[:person][:alt_phone],
+                      marital_status: params[:person][:marital_status], residential_address: params[:person][:residential_address],)
+      if @employee.update(employment_date: params[:person][:employment_date])
+        if @designated_role.update(designated_role: params[:designated_role])
+          redirect_to "/employees"
+          flash[:notice] = 'Successfully updated designation.'
+        end
+        flash[:notice] = 'Successfully updated employee.'
+      end
+      flash[:notice] = 'Successfully updated person.'
+    end
+  end
+
+  def can_access?
+    permitted_users = Designation.where(designated_role: ["Executive Director", "Administration Officer"]).pluck(:designation_id)
+    current_designation = EmployeeDesignation.where(employee_id: @current_user.id).pluck(:designation_id)
+    if (current_designation & permitted_users).empty?
+      flash[:error] = "You do not have permission to access this page."
+      redirect_to root_path
+    end
+  end
   def employee_params
     params.permit(:first_name, :middle_name, :last_name, :birth_date, :gender, :marital_status,
                                    :primary_phone, :alt_phone, :email_address, :postal_address, :official_email,
