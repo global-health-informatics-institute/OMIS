b6f29df4bda6c818380dc71cdcd923ae571a7b11
Thembi01 <sithembiphombeya@gmail.com>
2025-03-20 15:38:29 +0200

    activating emailing feature

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,13 +1,4 @@
-class RequisitionMailer < ApplicationMailer
-  default from: "omis@ghii.org"  # Ensure emails are sent from this address
-
-  def approval_email(requisition)
-    @requisition = requisition
-    @user = @requisition.requested_by  # Ensure requisition has a `requested_by` association
-
-    mail(
-      to: @user.email, 
-      subject: "Your Requisition ##{@requisition.id} Has Been Approved"
-    )
-  end
+class ApplicationMailer < ActionMailer::Base
+  default from: "ghii_omis@outlook.com"  # Ensure it matches your SMTP settings
+  layout "mailer"
 end
--- app/mailers/requisition_mailer.rb
+++ app/mailers/requisition_mailer.rb
@@ -1,2 +1,15 @@
 class RequisitionMailer < ApplicationMailer
 end
+class RequisitionMailer < ApplicationMailer
+  default from: "omis@ghii.org"  # Ensure emails are sent from this address
+
+  def funds_approved_email(requisition)
+    @requisition = requisition
+    @user = Person.find(@requisition.initiated_by) # Ensure requisition has a `requested_by` association
+
+    mail(
+      to: @user.email_address, 
+      subject: "Your Requisition ##{@requisition.id} Has Been Approved"
+    )
+  end
+end
