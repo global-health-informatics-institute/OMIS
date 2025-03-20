fd81f07c962c42898857c7b4ef665501aae4f264
Thembi01 <sithembiphombeya@gmail.com>
2025-03-20 03:43:22 +0200

    requisition_mailer mailer has modified

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,13 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: "omis@ghii.org"
-  layout "mailer"
+class RequisitionMailer < ApplicationMailer
+  default from: "omis@ghii.org"  # Ensure emails are sent from this address
+
+  def approval_email(requisition)
+    @requisition = requisition
+    @user = @requisition.requested_by  # Ensure requisition has a `requested_by` association
+
+    mail(
+      to: @user.email, 
+      subject: "Your Requisition ##{@requisition.id} Has Been Approved"
+    )
+  end
 end
