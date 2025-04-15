f9230b5028ca2078a431f8d814659aa1b7a0870c
Thembi01 <sithembiphombeya@gmail.com>
2025-04-15 10:22:02 +0200

    Refactor requisition review actions to include owner states and streamline query logic

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -201,11 +201,15 @@ class Employee < ApplicationRecord
     end
 
     # requisition reviews
-    actions += Requisition.where('workflow_state_id in (?) and initiated_by in (?)', WorkflowStateTransition
-                          .where(by_supervisor: true).collect { |x| x.workflow_state_id }, id)
-                          .collect do |x|
-      ["Review #{x.user.person.first_name}\'s #{x.requisition_type} requisition",
-       "/requisitions/#{x.id}"]
+    owner_states = WorkflowStateTransition.where(by_owner: true).pluck(:workflow_state_id)
+    #supervisor_states = WorkflowStateTransition.where(by_supervisor: true).pluck(:workflow_state_id)
+  
+    supervisor_states = WorkflowStateTransition.where(by_supervisor: true).pluck(:workflow_state_id)
+      actions += Requisition.where(
+        workflow_state_id: supervisor_states + owner_states,
+        initiated_by: current_supervisees.select(:supervisee)
+      ).map do |x|
+        ["Review #{x.user.person.first_name}'s #{x.requisition_type} requisition", "/requisitions/#{x.id}"]
     end
 
     actions += LeaveRequest.where('status in (?) and employee_id in (?)', WorkflowStateTransition
