9cc157122b7c8374f472cb5cbc03f7abb1074812
89Tain <manda.titani@gmail.com>
2025-04-16 15:17:16 +0200

    Add birthday snackbar helper method for notifications

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,6 +2,10 @@
 
 module NotificationsHelper # rubocop:disable Style/Documentation
   def notification_snackbar(messages, colors, duration: 3000) # rubocop:disable Metrics/MethodLength
+# app/helpers/notifications_helper.rb
+# app/helpers/notifications_helper.rb
+module NotificationsHelper
+  def birthday_snackbar(messages, duration: 3000, color: 'success') # rubocop:disable Metrics/MethodLength
     return if messages.blank?
 
     tag.div(
@@ -12,6 +16,8 @@ module NotificationsHelper # rubocop:disable Style/Documentation
         snackbar_messages_value: messages.to_json,
         snackbar_colors_value: colors.to_json,
         snackbar_duration_value: duration
+        snackbar_duration_value: duration,
+        snackbar_color_value: color
       }
     )
   end
