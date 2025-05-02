3f0f4ef8474714d9f8fba55308ba3f3f1f705149
martin-paul-hub <martinpaulmp104@gmail.com>
2025-05-02 13:39:08 +0200

    Define primary key and establish associations for supervisor and supervisee in Supervision model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,49 +2,55 @@ class ApplicationController < ActionController::Base
   before_action :logged_in?
   helper_method :current_user
   def current_user
-      # If session[:user_id] is nil, set it to nil, otherwise find the user by id.
-      @current_user ||= session[:user_id] && User.find_by(user_id: session[:user_id])
+    # If session[:user_id] is nil, set it to nil, otherwise find the user by id.
+    @current_user ||= session[:user_id] && User.find_by(user_id: session[:user_id])
   end
 
   def possible_actions(current_state, is_owner, is_supervisor)
-      actions = []
-      designation_ids = current_user.employee.current_designations.collect { |x| x.designation_id }
-      allowed_transitions = WorkflowStateActor.where(employee_designation_id:
-                                                       current_user.employee.current_designations.collect{|x| x.id})
-                                              .collect{|x| x.workflow_state_id}
+    actions = []
+    designation_ids = current_user.employee.current_designations.collect { |x| x.designation_id }
+    allowed_transitions = WorkflowStateActor.where(employee_designation_id:
+                                                     current_user.employee.current_designations.collect { |x| x.id })
+                                            .collect { |x| x.workflow_state_id }
 
-      #need to know possible actions, know things you are owner of, supervisor on, need your role
+    # need to know possible actions, know things you are owner of, supervisor on, need your role
 
-      (WorkflowStateTransition.where(workflow_state_id: current_state) || []).each do |transition|
-          if (is_owner && transition.by_owner) or (!is_owner && allowed_transitions.include?(transition.id))
-              actions.append(transition.action)
-          elsif (is_supervisor && transition.by_supervisor) and !is_owner
-              actions.append(transition.action)
-              elsif !is_owner && WorkflowStateActor.where(
-          workflow_state_id: current_state,
-          employee_designation_id: designation_ids
-        ).exists?
-          actions.append(transition.action)
-          end
+    (WorkflowStateTransition.where(workflow_state_id: current_state) || []).each do |transition|
+      next if transition.action == 'Rescind Request' && !is_owner
+
+      if (is_owner && transition.by_owner) or (!is_owner && allowed_transitions.include?(transition.id))
+        actions.append(transition.action)
+      elsif is_supervisor && transition.by_supervisor and !is_owner
+        actions.append(transition.action)
+      # Special case: allow only designation_id = 12 for workflow_state_id = 28
+      elsif !is_owner && current_state == 28 && designation_ids.include?(12)
+        actions.append(transition.action)
+      elsif !is_owner && WorkflowStateActor.where(
+        workflow_state_id: current_state,
+        employee_designation_id: designation_ids
+      ).exists?
+        actions.append(transition.action)
       end
-      # raise allowed_transitions.inspect
-      return  actions
+    end
+    # raise allowed_transitions.inspect
+    actions
   end
+
   def current_timesheet
-      unless @current_user.blank?
-          return Timesheet.where(employee_id: @current_user.employee.id,
-                                 timesheet_week: Date.today.beginning_of_week).first_or_create
-      end
+    return if @current_user.blank?
+
+    Timesheet.where(employee_id: @current_user.employee.id,
+                    timesheet_week: Date.today.beginning_of_week).first_or_create
   end
 
   def logged_in?
-      #function to check if user is logged in before accessing recourses
-      if current_user.blank?
-          redirect_to new_user_session_path
-      end
+    # function to check if user is logged in before accessing recourses
+    return unless current_user.blank?
+
+    redirect_to new_user_session_path
   end
 
   def can_access?
-      #function to check if user has access rights to the resource
+    # function to check if user has access rights to the resource
   end
 end
--- app/models/supervision.rb
+++ app/models/supervision.rb
@@ -1,2 +1,6 @@
 class Supervision < ApplicationRecord
+  self.primary_key = 'supervision_id'
+
+  belongs_to :supervisor_employee, foreign_key: 'supervisor', class_name: 'Employee'
+  belongs_to :supervisee_employee, foreign_key: 'supervisee', class_name: 'Employee'
 end
