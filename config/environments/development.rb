1e0a26394265f0e1eb1d84b18711b11908bbcade
martin-paul-hub <martinpaulmp104@gmail.com>
2025-05-15 09:36:46 +0200

    Refactor development environment configuration for consistency in string quoting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -19,13 +19,13 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
+  if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -57,27 +57,29 @@ Rails.application.configure do
     enable_starttls_auto: true,
     read_timeout: 120
   }
- 
-   config.action_mailer.raise_delivery_errors = true
- 
-   config.action_mailer.perform_caching = false
- 
-   config.action_mailer.delivery_method = :smtp
- 
-   config.action_mailer.default_options = { from: 'communications@ghii.org' }
-   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
- 
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_options = { from: 'communications@ghii.org' }
+  config.action_mailer.default_url_options = {
+    host: Rails.application.config.omis_url.gsub(':3000', ''),
+    port: 3000
+  }
+
   config.action_mailer.smtp_settings = {
-     address: 'smtp.gmail.com',
-     port: 587,
-     domain: 'gmail.com',
-     authentication: 'plain',
-     user_name: 'communications@ghii.org',
-     password: 'dleculgnoapizlzd',
-     enable_starttls_auto: true,
-     read_timeout: 120
-   }
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    authentication: 'plain',
+    user_name: 'communications@ghii.org',
+    password: 'dleculgnoapizlzd',
+    enable_starttls_auto: true,
+    read_timeout: 120
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
