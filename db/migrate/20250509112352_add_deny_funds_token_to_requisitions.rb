ed58bd42aa1cfdf3a7ff2d71e19e3873d862a827
martin-paul-hub <martinpaulmp104@gmail.com>
2025-05-20 13:01:49 +0200

    Remove deny funds token migration from requisitions for improved security

--- db/migrate/20250509112352_add_deny_funds_token_to_requisitions.rb
+++
@@ -1,6 +0,0 @@
-class AddDenyFundsTokenToRequisitions < ActiveRecord::Migration[7.0]
-  def change
-    add_column :requisitions, :deny_funds_token, :string
-    add_index :requisitions, :deny_funds_token, unique: true
-  end
-end
