ff001449308acb274b84b2c0d290759886cfeffd
martin-paul-hub <martinpaulmp104@gmail.com>
2025-05-20 13:03:19 +0200

    Update schema version and add reason field with unique indexes for requisitions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_05_09_112352) do
+ActiveRecord::Schema[7.0].define(version: 2025_05_13_082019) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -301,7 +301,10 @@ ActiveRecord::Schema[7.0].define(version: 2025_05_09_112352) do
     t.string "rejection_token"
     t.string "approval_funds_token"
     t.string "deny_funds_token"
+    t.text "reason"
+    t.index ["approval_funds_token"], name: "index_requisitions_on_approval_funds_token", unique: true
     t.index ["approval_token"], name: "index_requisitions_on_approval_token", unique: true
+    t.index ["deny_funds_token"], name: "index_requisitions_on_deny_funds_token", unique: true
     t.index ["rejection_token"], name: "index_requisitions_on_rejection_token", unique: true
   end
 
