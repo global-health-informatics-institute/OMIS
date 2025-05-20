f66a8314b9232e7926ee725122564f0593b50b5a
martin-paul-hub <martinpaulmp104@gmail.com>
2025-05-20 13:01:40 +0200

    Remove approval funds token migration from requisitions for improved security

--- db/migrate/20250509112242_add_approval_funds_token_to_requisitions.rb
+++
@@ -1,6 +0,0 @@
-class AddApprovalFundsTokenToRequisitions < ActiveRecord::Migration[7.0]
-  def change
-    add_column :requisitions, :approval_funds_token, :string
-    add_index :requisitions, :approval_funds_token, unique: true
-  end
-end
