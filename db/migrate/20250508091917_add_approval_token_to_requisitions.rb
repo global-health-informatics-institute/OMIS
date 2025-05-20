92ce481fc3d15f704182a7383e5dec33451c4293
martin-paul-hub <martinpaulmp104@gmail.com>
2025-05-20 13:01:25 +0200

    Remove approval token migration from requisitions for improved security

--- db/migrate/20250508091917_add_approval_token_to_requisitions.rb
+++
@@ -1,7 +0,0 @@
-class AddApprovalTokenToRequisitions < ActiveRecord::Migration[7.0]
-  def change
-    add_column :requisitions, :approval_token, :string
-    add_index :requisitions, :approval_token, unique: true
-
-  end
-end
