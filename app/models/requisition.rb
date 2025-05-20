e1d61230d5e468eba3814f2be20be8adab4acabf
martin-paul-hub <martinpaulmp104@gmail.com>
2025-05-20 12:37:01 +0200

    Comment out unused token generation methods in Requisition model

--- app/models/requisition.rb
+++ app/models/requisition.rb
@@ -1,11 +1,11 @@
 class Requisition < ApplicationRecord
-  before_create :generate_approval_token
-  before_create :generate_rejection_token 
-  before_create :generate_approval_funds_token
-  before_create :generate_deny_funds_token
+  # before_create :generate_approval_token
+  # before_create :generate_rejection_token 
+  # before_create :generate_approval_funds_token
+  # before_create :generate_deny_funds_token
   belongs_to :user, :foreign_key =>  :initiated_by
-  attribute :approval_token, :string
-  attribute :rejection_token, :string 
+  # attribute :approval_token, :string
+  # attribute :rejection_token, :string 
   # belongs_to :approver, class_name: 'User', :foreign_key =>  :approved_by
   belongs_to :project, foreign_key: :project_id
   has_many :requisition_items, :foreign_key => :requisition_id
@@ -45,31 +45,31 @@ class Requisition < ApplicationRecord
 
   private
 
-  def generate_approval_token
-    token = SecureRandom.urlsafe_base64(32)
-    Rails.logger.info "Requisition Model: Inside generate_approval_token. Token: #{token}"
-    self.approval_token = token
-    Rails.logger.info "Requisition Model: After assignment, self.approval_token: #{self.approval_token}"
-  end
-  def generate_rejection_token 
-    token = SecureRandom.urlsafe_base64(32)
-    Rails.logger.info "Requisition Model: Inside generate_rejection_token. Token: #{token}"
-    self.rejection_token = token
-    Rails.logger.info "Requisition Model: After assignment, self.rejection_token: #{self.rejection_token}"
-  end
-  def generate_approval_funds_token
-    token = SecureRandom.urlsafe_base64(32)
-    Rails.logger.info "Requisition Model: Inside generate_approval_funds_token. Token: #{token}"
-    self.approval_funds_token = token
-    Rails.logger.info "Requisition Model: After assignment, self.approval_funds_token: #{self.approval_funds_token}"
-  end
-  def generate_deny_funds_token
-    token = SecureRandom.urlsafe_base64(32)
-    Rails.logger.info "Requisition Model: Inside generate_deny_funds_token. Token: #{token}"
-    self.deny_funds_token = token
-    Rails.logger.info "Requisition Model: After assignment, self.deny_funds_token: #{self.deny_funds_token}"
-  end
+  # def generate_approval_token
+  #   token = SecureRandom.urlsafe_base64(32)
+  #   Rails.logger.info "Requisition Model: Inside generate_approval_token. Token: #{token}"
+  #   self.approval_token = token
+  #   Rails.logger.info "Requisition Model: After assignment, self.approval_token: #{self.approval_token}"
+  # end
+  # def generate_rejection_token 
+  #   token = SecureRandom.urlsafe_base64(32)
+  #   Rails.logger.info "Requisition Model: Inside generate_rejection_token. Token: #{token}"
+  #   self.rejection_token = token
+  #   Rails.logger.info "Requisition Model: After assignment, self.rejection_token: #{self.rejection_token}"
+  # end
+  # def generate_approval_funds_token
+  #   token = SecureRandom.urlsafe_base64(32)
+  #   Rails.logger.info "Requisition Model: Inside generate_approval_funds_token. Token: #{token}"
+  #   self.approval_funds_token = token
+  #   Rails.logger.info "Requisition Model: After assignment, self.approval_funds_token: #{self.approval_funds_token}"
+  # end
+  # def generate_deny_funds_token
+  #   token = SecureRandom.urlsafe_base64(32)
+  #   Rails.logger.info "Requisition Model: Inside generate_deny_funds_token. Token: #{token}"
+  #   self.deny_funds_token = token
+  #   Rails.logger.info "Requisition Model: After assignment, self.deny_funds_token: #{self.deny_funds_token}"
+  # end
   # def being_rejected?
-  #   workflow_state_id_changed? && WorkflowState.find_by(id: workflow_state_id)&.state == "Rejected"
+  #   workflow_state_id_changed? && WorkflowState.find_by(workflow_state_id: workflow_state_id)&.state == "Rejected"
   # end
 end
