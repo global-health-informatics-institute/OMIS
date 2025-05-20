1f6fd5fa5eadbe94f6fdcef5cade7491d715a5e6
martin-paul-hub <martinpaulmp104@gmail.com>
2025-05-20 13:01:32 +0200

    Remove rejection token migration from requisitions for improved security

--- db/migrate/20250509084846_add_rejection_token_to_requisitions.rb
+++
@@ -1,6 +0,0 @@
-class AddRejectionTokenToRequisitions < ActiveRecord::Migration[7.0]
-  def change
-    add_column :requisitions, :rejection_token, :string
-    add_index :requisitions, :rejection_token, unique: true
-  end
-end
