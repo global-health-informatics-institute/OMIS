25c4820cae2cd18b1907aafc8ce5f005962fb354
martin-paul-hub <martinpaulmp104@gmail.com>
2025-06-06 10:46:50 +0200

    Comment out alternative Bootstrap pin in importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.e
 pin '@popperjs/core', to: 'https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js'
 pin_all_from 'app/javascript/jquery', under: 'custom'
 pin 'canvas-confetti' # @1.9.3
+#pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
