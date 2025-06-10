14428e5d7a33ac1a30407c9e7258453ad80022cf
martin-paul-hub <martinpaulmp104@gmail.com>
2025-06-10 16:46:28 +0200

    Refactor petty cash transitions seed: move transaction block and optimize ID assignment for workflow state transitions

--- db/seeds/petty_cash_transitions_and_states.rb
+++ db/seeds/petty_cash_transitions_and_states.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-# bundle exec rails runner 'load "db/seeds/petty_cash_transitions_and_states.rb"'
-ActiveRecord::Base.transaction do
   liquidate_funds_workflow_state_transition = WorkflowStateTransition.find_by(action: 'Liquidate Funds')
   disburse_funds_workflow_state_transition= WorkflowStateTransition.find_by(action: 'Disburse Funds')
   liquidated_workflow_state = WorkflowState.find_by(workflow_state_id: 36)
+# bundle exec rails runner 'load "db/seeds/petty_cash_transitions_and_states.rb"'
+ActiveRecord::Base.transaction do
 
   if liquidated_workflow_state.nil?
     WorkflowState.create(
@@ -16,7 +16,9 @@ ActiveRecord::Base.transaction do
     )
   end
   if liquidate_funds_workflow_state_transition.nil?
+    last_id = WorkflowStateTransition.maximum(:id) || 0
     WorkflowStateTransition.create(
+      id: last_id + 1,
       workflow_state_id: 29,
       next_state: 36,
       voided: false,
@@ -26,7 +28,9 @@ ActiveRecord::Base.transaction do
     )
   end
   if disburse_funds_workflow_state_transition.nil?
+    last_id = WorkflowStateTransition.maximum(:id) || 0
     WorkflowStateTransition.create(
+      id: last_id + 1,
       workflow_state_id: 28,
       next_state: 29,
       voided: false,
