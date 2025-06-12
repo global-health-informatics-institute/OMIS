738d86d86e322240021d4086aba031bcbc3b9578
martin-paul-hub <martinpaulmp104@gmail.com>
2025-06-12 14:58:51 +0200

    Refactor Requisition model: add quantity method, clean up unused code, and improve attribute handling

--- app/models/requisition.rb
+++ app/models/requisition.rb
@@ -6,6 +6,12 @@ class Requisition < ApplicationRecord
   has_many :petty_cash_comments, foreign_key: :requisition_id
   belongs_to :workflow_state, foreign_key: :workflow_state_id, primary_key: :workflow_state_id, optional: true
   has_and_belongs_to_many :employees
+  #accessing the virtual attribute
+  attr_accessor :item_requested
+  #accepting nested attributes for requisition items
+  accepts_nested_attributes_for :requisition_items
+
+
   def assign_state
     self.workflow_state_id = InitialState.find_by_workflow_process_id(WorkflowProcess.find_by_workflow('Petty Cash Request')).workflow_state_id
   end
@@ -27,6 +33,11 @@ class Requisition < ApplicationRecord
   def reviewer
     Employee.find(self.reviewed_by).person.full_name rescue nil # Handle potential nil value
   end
+  def quantity
+    if self.requisition_type == "Purchase Request"
+      return self.requisition_items.first.quantity
+    end
+  end
 
   def amount
     if self.requisition_type == "Petty Cash"
@@ -35,22 +46,9 @@ class Requisition < ApplicationRecord
   end
  def used_amount
     if self.requisition_type == "Petty Cash"
-      # This will return nil if petty_cash_comments.first is nil,
-      # preventing the error.
       return self.petty_cash_comments.first&.used_amount || 'Missing'
     end
     # nil # Or 0, or some default value if not a "Petty Cash" requisition
   end
   private
-
-  # def generate_approval_token
-  #   token = SecureRandom.urlsafe_base64(32)
-  #   Rails.logger.info "Requisition Model: Inside generate_approval_token. Token: #{token}"
-  #   self.approval_token = token
-  #   Rails.logger.info "Requisition Model: After assignment, self.approval_token: #{self.approval_token}"
-  # end
-  # def generate_rejection_token 
-  #   token = SecureRandom.urlsafe_base64(32)
-  #   Rails.logger.info "Requisition Model: Inside generate_rejection_token. Token: #{token}"
-
 end
