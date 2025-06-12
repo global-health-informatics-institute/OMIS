19e1430178bc899994178f85577fbcf09418e332
martin-paul-hub <martinpaulmp104@gmail.com>
2025-06-12 15:40:14 +0200

    Refactor petty cash workflow seeds: add missing workflow states and transitions for purchase requests

--- db/seeds/petty_cash_transitions_and_states.rb
+++ db/seeds/petty_cash_transitions_and_states.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
+# bundle exec rails runner 'load "db/seeds/petty_cash_transitions_and_states.rb"'
 
   liquidate_funds_workflow_state_transition = WorkflowStateTransition.find_by(action: 'Liquidate Funds')
   disburse_funds_workflow_state_transition= WorkflowStateTransition.find_by(action: 'Disburse Funds')
   liquidated_workflow_state = WorkflowState.find_by(workflow_state_id: 36)
-# bundle exec rails runner 'load "db/seeds/petty_cash_transitions_and_states.rb"'
+  purchase_request_workflow_process = WorkflowProcess.find_by(workflow: 'Purchase Request')
+  approve_purchase_request_workflow_state_transition = WorkflowStateTransition.find_by(action: 'Approve Purchase Request')
+  requested_purchase_request_workflow_state = WorkflowState.find_by(workflow_state_id: 37)
+  approved_purchase_request_workflow_state = WorkflowState.find_by(workflow_state_id: 38)
 ActiveRecord::Base.transaction do
 
   if liquidated_workflow_state.nil?
@@ -13,6 +17,31 @@ ActiveRecord::Base.transaction do
       state: 'Liquidated',
       description: 'Petty cash funds have been liquidated',
       voided: false
+    )
+  end
+  if requested_purchase_request_workflow_state.nil?
+    WorkflowState.create(
+      workflow_state_id: 37,
+      workflow_process_id: 6,
+      state: 'Requested',
+      description: 'Purchase request has been initiated',
+      voided: false
+    )
+  end
+  if approved_purchase_request_workflow_state.nil?
+    WorkflowState.create(
+      workflow_state_id: 38,
+      workflow_process_id: 6,
+      state: 'Approved',
+      description: 'Purchase request has been approved',
+      voided: false
+    )
+  end
+  if purchase_request_workflow_process.nil?
+    WorkflowProcess.create(
+      workflow_process_id: 6,
+      workflow: 'Purchase Request',
+      active: true
     )
   end
   if liquidate_funds_workflow_state_transition.nil?
@@ -37,6 +66,18 @@ ActiveRecord::Base.transaction do
       action: 'Disburse Funds',
       by_owner: false,
       by_supervisor: false
+    )
+  end
+   if approve_purchase_request_workflow_state_transition.nil?
+    last_id = WorkflowStateTransition.maximum(:id) || 0
+    WorkflowStateTransition.create(
+      id: last_id + 1,
+      workflow_state_id: 37,
+      next_state: 38,
+      voided: false,
+      action: 'Approve Purchase Request',
+      by_owner: false,
+      by_supervisor: true
     )
   end
 end
