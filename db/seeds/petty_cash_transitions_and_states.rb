a6ac72e841de9fe4f81de872cfc446f9aca2f9f2
martin-paul-hub <martinpaulmp104@gmail.com>
2025-06-16 08:38:22 +0200

    Refactor petty cash transitions and states seed file: improve formatting and ensure initial state creation for purchase requests

--- db/seeds/petty_cash_transitions_and_states.rb
+++ db/seeds/petty_cash_transitions_and_states.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
+
 # bundle exec rails runner 'load "db/seeds/petty_cash_transitions_and_states.rb"'
 
-  liquidate_funds_workflow_state_transition = WorkflowStateTransition.find_by(action: 'Liquidate Funds')
-  disburse_funds_workflow_state_transition= WorkflowStateTransition.find_by(action: 'Disburse Funds')
-  liquidated_workflow_state = WorkflowState.find_by(workflow_state_id: 36)
-  purchase_request_workflow_process = WorkflowProcess.find_by(workflow: 'Purchase Request')
-  approve_purchase_request_workflow_state_transition = WorkflowStateTransition.find_by(action: 'Approve Purchase Request')
-  requested_purchase_request_workflow_state = WorkflowState.find_by(workflow_state_id: 37)
-  approved_purchase_request_workflow_state = WorkflowState.find_by(workflow_state_id: 38)
+liquidate_funds_workflow_state_transition = WorkflowStateTransition.find_by(action: 'Liquidate Funds')
+disburse_funds_workflow_state_transition = WorkflowStateTransition.find_by(action: 'Disburse Funds')
+liquidated_workflow_state = WorkflowState.find_by(workflow_state_id: 36)
+purchase_request_workflow_process = WorkflowProcess.find_by(workflow: 'Purchase Request')
+approve_purchase_request_workflow_state_transition = WorkflowStateTransition.find_by(action: 'Approve Purchase Request')
+requested_purchase_request_workflow_state = WorkflowState.find_by(workflow_state_id: 37)
+approved_purchase_request_workflow_state = WorkflowState.find_by(workflow_state_id: 38)
+initial_state_purchase_request = InitialState.find_by(workflow_process_id: 6)
 ActiveRecord::Base.transaction do
-
   if liquidated_workflow_state.nil?
     WorkflowState.create(
       workflow_state_id: 36,
@@ -17,6 +18,12 @@ ActiveRecord::Base.transaction do
       state: 'Liquidated',
       description: 'Petty cash funds have been liquidated',
       voided: false
+    )
+  end
+  if initial_state_purchase_request.nil?
+    InitialState.create(
+      workflow_process_id: 6,
+      workflow_state_id: 37
     )
   end
   if requested_purchase_request_workflow_state.nil?
@@ -68,7 +75,7 @@ ActiveRecord::Base.transaction do
       by_supervisor: false
     )
   end
-   if approve_purchase_request_workflow_state_transition.nil?
+  if approve_purchase_request_workflow_state_transition.nil?
     last_id = WorkflowStateTransition.maximum(:id) || 0
     WorkflowStateTransition.create(
       id: last_id + 1,
