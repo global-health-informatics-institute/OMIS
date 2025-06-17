a0cb3f57b30699a30c409a47e533011ed2a8860c
martin-paul-hub <martinpaulmp104@gmail.com>
2025-06-17 14:01:49 +0200

    Refactor purchase_request_threshold method in GlobalProperty model for clarity and consistency

--- app/models/global_property.rb
+++ app/models/global_property.rb
@@ -5,5 +5,12 @@ class GlobalProperty < ApplicationRecord
       record = find_by(property: 'petty.cash.limit')
       record ? record.property_value.to_f : 40_000
     end
-end
+  end
+  def self.purchase_request_threshold
+    Rails.cache.fetch('global_properties/purchase_request_threshold') do 
+      record = find_by(property: 'purchase.request.threshold')
+      record ? record.property_value.to_f : 3000000
+    end
+  end
+
 end
