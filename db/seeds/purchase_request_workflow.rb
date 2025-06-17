b21ec9e0c824916151657b998ebe8965d2a24b4a
martin-paul-hub <martinpaulmp104@gmail.com>
2025-06-17 10:31:13 +0200

    Add workflow states and transitions for procurement process in seed file

--- db/seeds/purchase_request_workflow.rb
+++ db/seeds/purchase_request_workflow.rb
@@ -10,7 +10,11 @@ initial_state_purchase_request = InitialState.find_by(workflow_process_id: 6)
 admin_designation_workflow_state_actor = WorkflowStateActor.find_by(workflow_state_id: 38)
 rejected_state_purchase_request = WorkflowState.find_by(workflow_state_id:39)
 reject_purchase_request_workflow_state_transition = WorkflowStateTransition.find_by(next_state: 39)
-
+under_procurement_workflow_state = WorkflowState.find_by(workflow_state_id: 40)
+start_procurement_workflow_state_transition = WorkflowStateTransition.find_by(next_state: 40)
+complete_procurement_workflow_state_transition = WorkflowStateTransition.find_by(next_state: 41)
+procured_purchase_request_workflow_state = WorkflowState.find_by(workflow_state_id: 41)
+admin_designation_under_procurement_workflow_state_actor = WorkflowStateActor.find_by(workflow_state_id: 40)
 
 ActiveRecord::Base.transaction do
   if initial_state_purchase_request.nil?
@@ -19,12 +23,22 @@ ActiveRecord::Base.transaction do
       workflow_state_id: 37
     )
   end
+  # workflow_state_model
   if requested_purchase_request_workflow_state.nil?
     WorkflowState.create(
       workflow_state_id: 37,
       workflow_process_id: 6,
       state: 'Requested',
       description: 'Purchase request has been initiated',
+      voided: false
+    )
+  end
+  if under_procurement_workflow_state.nil?
+    WorkflowState.create(
+      workflow_state_id: 40,
+      workflow_process_id: 6,
+      state: 'Under Procurement',
+      description: 'Purchase request is under procurement',
       voided: false
     )
   end
@@ -37,14 +51,16 @@ ActiveRecord::Base.transaction do
       voided: false
     )
   end
-  if admin_designation_workflow_state_actor.nil?
-    WorkflowStateActor.create(
-      workflow_state_id:38,
-      employee_designation_id:12,
+  if procured_purchase_request_workflow_state.nil?
+    WorkflowState.create(
+      workflow_state_id: 41,
+      workflow_process_id: 6,
+      state: 'Procured',
+      description: 'Purchase request has been procured',
       voided: false
     )
   end
-  if approved_purchase_request_workflow_state.nil?
+    if approved_purchase_request_workflow_state.nil?
     WorkflowState.create(
       workflow_state_id: 38,
       workflow_process_id: 6,
@@ -53,6 +69,22 @@ ActiveRecord::Base.transaction do
       voided: false
     )
   end
+  # workflow_state_actors_model
+  if admin_designation_workflow_state_actor.nil?
+    WorkflowStateActor.create(
+      workflow_state_id:38,
+      employee_designation_id:12,
+      voided: false
+    )
+  end
+  if admin_designation_under_procurement_workflow_state_actor.nil?
+    WorkflowStateActor.create(
+      workflow_state_id: 40,
+      employee_designation_id: 12,
+      voided: false
+    )
+  end
+  # workflow_state_transitions_model
   if purchase_request_workflow_process.nil?
     WorkflowProcess.create(
       workflow_process_id: 6,
@@ -82,6 +114,30 @@ ActiveRecord::Base.transaction do
       action: 'Reject Request',
       by_owner: false,
       by_supervisor: true
+    )
+  end
+  if start_procurement_workflow_state_transition.nil?
+    last_id = WorkflowStateTransition.maximum(:id) || 0
+    WorkflowStateTransition.create(
+      id: last_id + 1,
+      workflow_state_id: 38,
+      next_state: 40,
+      voided: false,
+      action: 'Start Procurement',
+      by_owner: false,
+      by_supervisor: false
+    )
+  end
+  if complete_procurement_workflow_state_transition.nil?
+    last_id = WorkflowStateTransition.maximum(:id) || 0
+    WorkflowStateTransition.create(
+      id: last_id + 1,
+      workflow_state_id: 40,
+      next_state: 41,
+      voided: false,
+      action: 'Complete Procurement',
+      by_owner: false,
+      by_supervisor: false
     )
   end
 end
