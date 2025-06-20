10eb0c195caf5fe5c21ac408e0bfdcbf359deaf5
martin-paul-hub <martinpaulmp104@gmail.com>
2025-06-20 11:52:02 +0200

    Fix duplicate belongs_to association for requisition in PurchaseRequestAttachment model

--- app/models/purchase_request_attachment.rb
+++ app/models/purchase_request_attachment.rb
@@ -2,4 +2,5 @@ class PurchaseRequestAttachment < ApplicationRecord
   belongs_to :requisition
   belongs_to :department, foreign_key: :department_id
   belongs_to :requisition, foreign_key: :requisition_id
+  belongs_to :stakeholder, foreign_key: :stakeholder_id
 end
