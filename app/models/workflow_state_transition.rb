07f0881fdaad9325082b50cde5751a7015921cfe
89Tain <manda.titani@gmail.com>
2025-06-25 15:44:49 +0200

    Add default scope to include only non-voided workflow state transitions

--- app/models/workflow_state_transition.rb
+++ app/models/workflow_state_transition.rb
@@ -3,7 +3,7 @@ class WorkflowStateTransition < ApplicationRecord
   has_one :workflow_state, :foreign_key => :next_state
   has_one :workflow_state, :foreign_key => :workflow_state_id
   has_one :workflow_process, through: :workflow_state
-  # default_scope { where(voided: false) }
+  default_scope { where(voided: false) }
   def self.possible_actions(state, user, is_owner= false)
     actions = []
 
