b7ddfbd8b1170c2039c757a45cad0e93b8c0a6b9
martin-paul-hub <martinpaulmp104@gmail.com>
2025-07-01 10:26:14 +0200

    Add validations for category presence and uniqueness in AssetCategory model

--- app/models/asset_category.rb
+++ app/models/asset_category.rb
@@ -1,4 +1,6 @@
 class AssetCategory < ApplicationRecord
   validates_presence_of :category, :description
   validates_uniqueness_of :category
+  has_many :assets 
+  validates :category, presence: true, uniqueness: true
 end
