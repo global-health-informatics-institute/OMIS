4377d0515e5f667acaeb184087fceef380be82c1
martin-paul-hub <martinpaulmp104@gmail.com>
2025-07-01 10:26:29 +0200

    Refactor GlobalProperty model to improve method structure and consistency for fetching global properties

--- app/models/global_property.rb
+++ app/models/global_property.rb
@@ -5,5 +5,33 @@ class GlobalProperty < ApplicationRecord
       record = find_by(property: 'petty.cash.limit')
       record ? record.property_value.to_f : 40_000
     end
-end
+  end
+
+  def self.lunch_allowance
+    Rails.cache.fetch('global_properties/lunch_allowance', expires_in: 1.hour) do
+      record = find_by(property: 'per.diem.lunch')
+      record ? record.property_value.to_f : 0
+    end
+  end
+
+  def self.dinner_allowance
+    Rails.cache.fetch('global_properties/dinner_allowance') do
+      record = find_by(property: 'per.diem.dinner')
+      record ? record.property_value.to_f : 0
+    end
+  end
+
+  def self.accommodation
+    Rails.cache.fetch('global_properties/accommodation') do
+      record = find_by(property: 'per.diem.accommodation')
+      record ? record.property_value.to_f : 0
+    end
+  end
+
+  def self.unit_fuel_cost
+    Rails.cache.fetch('global_properties/unit_fuel_cost', expires_in: 1.hour) do
+      record = find_by(property: 'unit.fuel.cost')
+      record ? record.property_value.to_f : 0
+    end
+  end
 end
