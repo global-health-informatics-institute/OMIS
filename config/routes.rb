b75330c5a6b9d8cbff4f38634feae0175d339deb
martin-paul-hub <martinpaulmp104@gmail.com>
2025-07-14 16:01:20 +0200

    Update procurement routes to reflect IPC requirements and clarify actions

--- config/routes.rb
+++ config/routes.rb
@@ -42,10 +42,10 @@ Rails.application.routes.draw do
   get 'purchase_request/show', to: 'purchase_request#show'
   put 'purchase_requests/:id/approve_request', to: 'purchase_requests#approve_request'
   put 'purchase_requests/:id/reject_request', to: 'purchase_requests#reject_request'
-  put 'purchase_requests/:id/start_procurement', to: 'purchase_requests#start_procurement'
+  put 'purchase_requests/:id/source_quotations', to: 'purchase_requests#source_quotations'
   #put 'purchase_requests/:id/request_payment', to: 'purchase_requests#request_payment'
   match 'purchase_requests/:id/request_payment', to: 'purchase_requests#request_payment', via: [:patch, :put], as: :complete_procurement_purchase_request
-  put 'purchase_requests/:id/appeal_request', to: 'purchase_requests#appeal_request'
+  put 'purchase_requests/:id/require_ipc', to: 'purchase_requests#require_ipc'
   put 'purchase_requests/:id/approve_funds', to: 'purchase_requests#approve_funds'
   put 'purchase_requests/:id/deny_funds', to: 'purchase_requests#deny_funds'
 
