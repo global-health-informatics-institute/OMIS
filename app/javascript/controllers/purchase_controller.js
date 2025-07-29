e4ec8b24b154fa7422b8df186442cea41cfc501a
martin-paul-hub <martinpaulmp104@gmail.com>
2025-07-29 14:30:41 +0200

    Debug amount field when reodering steps

--- app/javascript/controllers/purchase_controller.js
+++ app/javascript/controllers/purchase_controller.js
@@ -262,8 +262,8 @@ export default class extends Controller {
   // New method to control the visibility of next and previous buttons
   updateButtonVisibility() {
     console.log("Updating Next and Previous button visibility based on current state.");
-    const shouldShowButtons = this.currentStateValue === "Payment Requested";
-    console.log("Current state value is:", this.currentStateValue, "Should show buttons?", this.currentStateValue === "Payment Requested")
+    const shouldShowButtons = this.currentStateValue === "Pending Payment Request" || this.currentStateValue === "Payment Requested";
+    console.log("Current state value is:", this.currentStateValue, "Should show buttons?", shouldShowButtons);
     console.log("Step index:", this.currentStepValue, "Total:", this.visiblePanels.length);
 
 
