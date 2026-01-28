5aa6f72df0af5c44c8409d492dabe2ecd74f6bd4
89Tain <manda.titani@gmail.com>
2026-01-28 15:40:51 +0200

    feat: establish relationships between Donor and Project models through donor_projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,9 @@ class Project < ApplicationRecord
   belongs_to :employee, :foreign_key => :manager
   has_many :project_tasks
 
+  has_many :donor_projects
+  has_many :donors, through: :donor_projects
+
   def upcoming_deadlines
     return ProjectTask.where("deadline >= ?", Date.today)
   end
