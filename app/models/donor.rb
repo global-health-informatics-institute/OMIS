4c30bd5f4b3d458c317844699a1bd6d15aeda396
89Tain <manda.titani@gmail.com>
2026-01-28 15:40:51 +0200

    feat: establish relationships between Donor and Project models through donor_projects

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -2,5 +2,8 @@
 
 # Model representing a donor entity.
 class Donor < ApplicationRecord
+  has_many :donor_projects
+  has_many :projects, through: :donor_projects
+
   scope :active, -> { where(voided: false) }
 end
