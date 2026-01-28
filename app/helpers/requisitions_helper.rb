90a0ac7e252a470a66607cf01e410e2e67bf21a6
89Tain <manda.titani@gmail.com>
2026-01-28 10:46:42 +0200

    chore: format options array in select_requisition method for improved readability

--- app/helpers/requisitions_helper.rb
+++ app/helpers/requisitions_helper.rb
@@ -1,13 +1,14 @@
 module RequisitionsHelper
   def select_requisition
-    options = [['Petty Cash Request','Petty Cash' ], ['Asset Request', 'Asset Request'],
-               ['Purchase Request', 'Purchase Request'],
-               ['Travel Requisition','Travel Request',],
-               ['Personnel Requests','Personnel Request'],
-               ['Leave Request', 'Leave Request'],
-               ['Token Request', 'Token Request'],
+    options = [
+      ['Petty Cash Request', 'Petty Cash'],
+      ['Asset Request', 'Asset Request'],
+      ['Purchase Request', 'Purchase Request'],
+      ['Travel Requisition', 'Travel Request'],
+      ['Personnel Requests', 'Personnel Request'],
+      ['Leave Request', 'Leave Request'],
+      ['Token Request', 'Token Request']
     ]
-    return options
   end
 
 
