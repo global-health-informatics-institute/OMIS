9210a314f2e08cdf1c56e699ca424ec0d44d6bda
89Tain <manda.titani@gmail.com>
2026-02-18 09:52:23 +0200

    fix: point to the correct primary key 'requisition_id' not `id`

--- app/models/purchase_request_detail.rb
+++ app/models/purchase_request_detail.rb
@@ -2,7 +2,7 @@
 
 # Holds specific metadata for Requisitions of type 'purchase_request'.
 class PurchaseRequestDetail < ApplicationRecord
-  belongs_to :requisition
+  belongs_to :requisition, foreign_key: 'requisition_id', optional: false
   belongs_to :department, foreign_key: 'department_id', optional: false
   belongs_to :donor, optional: true
 
