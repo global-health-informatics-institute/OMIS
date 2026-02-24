bc75702813cbf616bf8c208685a0681df7f63883
89Tain <manda.titani@gmail.com>
2026-02-24 15:58:50 +0200

    feat: rename budget_line association to requisition_budget_line in Requisition model

--- app/models/requisition_budget_line.rb
+++ app/models/requisition_budget_line.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
+# Model representing the association between a Requisition and a BudgetLine.
 class RequisitionBudgetLine < ApplicationRecord
-  belongs_to :requisition
+  belongs_to :requisition, foreign_key: 'requisition_id', optional: false
   belongs_to :budget_line
+
+  scope :active, -> { where(voided: false) }
 end
