063a7b1c6dd57da4fccf486eb29eba19b4c39093
89Tain <manda.titani@gmail.com>
2026-06-02 14:52:39 +0200

    fix(BudgetLine): correct associations to use has_many through RequisitionBudgetLine

--- app/models/budget_line.rb
+++ app/models/budget_line.rb
@@ -1,3 +1,6 @@
-class BudgetLine < ApplicationRecord
-  belongs_to :requisition
+# frozen_string_literal: true
+
+class BudgetLine < ApplicationRecord # rubocop:disable Style/Documentation
+  has_many :requisition_budget_lines
+  has_many :requisitions, through: :requisition_budget_lines
 end
