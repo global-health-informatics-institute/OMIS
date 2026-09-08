4caa9b22433d0654800599774efc529f24172330
Titani Manda <manda.titani@gmail.com>
2026-09-09 00:44:47 +0200

    chore(purchase_request_details):  resolve rubocop syntax warning

--- db/migrate/20260908224204_add_approved_on_to_purchase_request_details.rb
+++ db/migrate/20260908224204_add_approved_on_to_purchase_request_details.rb
@@ -1,4 +1,6 @@
-class AddApprovedOnToPurchaseRequestDetails < ActiveRecord::Migration[7.0]
+# frozen_string_literal: true
+
+class AddApprovedOnToPurchaseRequestDetails < ActiveRecord::Migration[7.0] # rubocop:disable Style/Documentation
   def change
     add_column :purchase_request_details, :approved_on, :datetime
   end
